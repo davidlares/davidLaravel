c215f5141b848d216eadb79bbf224b29c8fb4a7d
David Lares <david.e.lares@gmail.com>
2018-09-21 17:30:56 -0400

    Initial API Structure

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::resource('users','User\UserController', ['except' => ['create','edit']]);
+Route::resource('transactions','Transaction\TransactionController', ['only' => ['index','show']]);
+Route::resource('sellers','Seller\SellerController', ['only' => ['index','show']]);
+Route::resource('buyers','Buyer\BuyerController', ['only' => ['index','show']]);
+Route::resource('products','Product\ProductController', ['only' => ['index','show']]);
+Route::resource('categories','Category\CategoryController', ['except' => ['create','edit']]);
