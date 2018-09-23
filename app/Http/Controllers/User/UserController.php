658e948289721b6162d912d6cd27fdf6124c4ff2
David Lares <david.e.lares@gmail.com>
2018-09-22 23:19:13 -0400

    Users Functionality

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
 
 class UserController extends Controller
 {
@@ -14,17 +15,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $users = User::all();
+        // return root (index) + status
+        return response()->json(['data' => $users], 200);
     }
 
     /**
@@ -35,7 +28,15 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $fields = $request->all();
+        $fields['password'] = bcrypt($request->password);
+        $fields['verified'] = User::NOT_VERIFIED;
+        $fields['verification_token'] = User::generateVerificationToken();
+        $fields['admin'] = User::REGULAR;
+
+        $user = User::create($fields);
+        return response()->json(['data' => $user], 201);
     }
 
     /**
@@ -46,18 +47,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $user = User::findOrFail($id);
+        return response()->json(['data' => $user], 200);
     }
 
     /**
@@ -69,7 +60,46 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $user = User::findOrFail($id);
+
+      $this->validate($request,[
+        'email' => 'email|unique:users,email,'. $user->id,
+        'password' => 'min:6|confirmed',
+        'admin' => 'in: '. User::ADMIN. ',' . User::REGULAR,
+      ]);
+
+      if($request->has('name')){
+        $user->name = $request->name;
+      }
+
+      if($request->has('email')){
+        if($user->email != $request->email){
+          $user->verified = User::NOT_VERIFIED;
+          $user->verification_token = User::generateVerificationToken();
+          $user->email = $request->email;
+        }
+      }
+
+      if($request->has('password')){
+        $user->password = bcrypt($request->password);
+      }
+
+      if($request->has('admin')){
+        if(!$user->isVerified()){
+          return response()->json(['error' => 'Verified users can only change their admin status', 'code' => 409], 409);
+        }
+        $user->admin = $request->admin;
+      }
+
+      // need to change something for update the record
+
+      if(!$user->isDirty()){
+        return response()->json(['error' => 'Must changed at least one text field to update', 'code' => 422], 422);
+      }
+
+      $user->save();
+      return response()->json(['data' => $user], 200);
+
     }
 
     /**
@@ -80,6 +110,8 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json(['data', $user], 200);
     }
 }
